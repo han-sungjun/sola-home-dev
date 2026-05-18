190683f6ded023dc814c40a01760c41bc2f4166e
han-sungjun <sungjun200@gmail.com>
2026-05-18 15:49:14 +0900

    수정
    수정

--- js/phone-verify-core.js
+++ js/phone-verify-core.js
@@ -630,8 +630,8 @@ function updatePasswordResetUi() {
   }
 
   const canSubmit =
-    pw.length >= 6 &&
-    confirm.length >= 6 &&
+    pw.length >= 8 &&
+    confirm.length >= 8 &&
     pw === confirm &&
     strength !== 'weak';
 
